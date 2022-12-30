3abf1f0ee0996c8e6f7dfaf5724a0404bea51d7c
Sravan NG <Sravan NG@LAPTOP-RDPPE8VG>
2022-12-30 12:17:40 +0530

    project_demo

---
+++ projectdemogithub/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Sravan NG
+ *
+ */
+module projectdemogithub {
+}
---
+++ projectdemogithub/src/projectdemogithub/driverclass.java
@@ -0,0 +1,9 @@
+package projectdemogithub;
+
+public class driverclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello github");
+	}
+}
